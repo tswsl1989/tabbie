441f1c528a42a12e922519d184f4b95a955b6b67
klaas <klaas@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-06-20 21:32:25 +0000

    Minor refactorings
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@35 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- www/draw.php
+++ www/draw.php
@@ -1,41 +1,15 @@
 <?
-/******************************************************************************
-File    :   draw.php
+require_once("includes/dbconnection.php");
 
-Author  :   Deepak Jois
+$moduletype = trim(@$_GET['moduletype']); //module type : round currentdraw
 
-Purpose :   This file manages all the functions of the draw, including creation
-            and display of the draw.
+$query = "SHOW TABLES LIKE 'draw_round%'";
+$result = mysql_query($query);
+$numdraws = mysql_num_rows($result);
+$nextround = $numdraws + 1;
 
-******************************************************************************/
+if ($moduletype != "currentdraw" && $moduletype != "manualdraw")
+    $moduletype="round"; 
 
-
-include("includes/dbconnection.php"); //Database Connection
-
-$moduletype=trim(@$_GET['moduletype']); //module type : round currentdraw
-if (!$moduletype) $moduletype="round"; //set to round if empty
-
-
-//Check Database
-$query="SHOW TABLES LIKE 'draw_round%'";
-$result=mysql_query($query);
-$numdraws=mysql_num_rows($result);
-
-//Calculate Next Round
-$nextround=$numdraws+1;
-
-
-switch($moduletype)
-{
-    case "round":
-    case "currentdraw":
-    case "manualdraw":
-                        break;
-
-    default:
-                        $moduletype="round"; 
-}
-
-//Load respective module
-include("draw/$moduletype.inc");
-?>          
+require("draw/$moduletype.inc");
+?>

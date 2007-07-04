e68cff8dd20a69be8321e3262a4443030e269e91
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-07-04 22:16:13 +0000

    Started creating some clarity in data structures
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@79 fa58ba4a-40fe-4f97-9c31-9099acd41d38

---
+++ run/draw/algorithms/minimal_algorithm.php
@@ -0,0 +1,26 @@
+<?php
+
+//typical input value:
+$teams = array(
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+    array("id" => "1", "points" => 0, "positions" => array(0, 0, 0, 1)),
+);
+
+function calculate_draw($teams) {
+
+
+
+}
+
+//typical output value:
+
+$debates = array(
+    array("1", "2", "3", "4"),
+    array("5", "6", "7", "8"));
+
+?>

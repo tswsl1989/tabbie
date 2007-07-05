389fbd520c07494fad7f6333990a88a1595c0aae
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-07-05 18:18:09 +0000

    Half-way check in - end of day
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@89 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- run/draw/algorithms/silver_line.php
+++ run/draw/algorithms/silver_line.php
@@ -11,26 +11,17 @@ function team_badness(&$team, $position = -1) {
     return badness($positions);
 }
 
-function debate_badness(&$debates) {
+function teams_badness(&$teams) {
     $result = 0;
-    $current_position = 0;
-    foreach ($debates as $team) {
-        $result += team_badness($team, $current_position);
-        $current_position++;
-    }
-    return $result;
-}
-
-function debates_badnesses(&$debates) {
-    $result = 0;
-    foreach ($debates as $debate) {
-        $result += debate_badness($debate);
+    foreach ($teams as $team) {
+        $result += team_badness($team);
     }
     return $result;
 }
 
 function attributed_teams_from_debates(&$debates) {
     $result = array();
+    $index = 0;
     foreach ($debates as $debate) {
         $current_position = 0;
         $best_team = $debate[0];
@@ -38,67 +29,65 @@ function attributed_teams_from_debates(&$debates) {
         foreach ($debate as $team) {
             $attributed_team = $team;
             $attributed_team["current_position"] = $current_position;
-            $attributed_team["debate_level"] = $debate_index;
-            $current_position++;
+            $attributed_team["debate_level"] = $debate_level;
+            $attributed_team["index"] = $index;
             $result[] = $attributed_team;
+            $current_position++;
+            $index++;
         }
-        $debate_index++;
     }
     return $result;
 }
-/*
-    def teamsInPosition(self):
-        result = []
-        for debate in self.debates:
-            for position, team in enumerate(debate.positions):
-                result.append(PositionedTeam(team, position, debate))
-        return result
-*/
+
 function is_swappable($team_a, $team_b) {
     return
         ($team_a["team_id"] != $team_b["team_id"]) &&
         (($team_a["points"] == $team_b["points"]) ||
-        ($team_a["debate_level"] == $team_b["debate_level"]))
+        ($team_a["debate_level"] == $team_b["debate_level"]));
 }
 
-/*
-def swapTwoTeams(teamInPositionA, teamInPositionB):
-    debateA = teamInPositionA.debate
-    debateB = teamInPositionB.debate
-    positionA = teamInPositionA.position
-    positionB = teamInPositionB.position
-    debateA.positions[positionA] = teamInPositionB.team
-    debateB.positions[positionB] = teamInPositionA.team
-    teamInPositionA.position = positionB
-    teamInPositionB.position = positionA
-    teamInPositionA.debate = debateB
-    teamInPositionB.debate = debateA
-
-*/
-function swap_two_teams(&$team_a, &$team_b) {
-    print $team_a['team_id'] . " ". $team_b['team_id'] . "<br>";
+function swap_two_teams(&$teams, &$team_a, &$team_b) {
+    $current_position_a = $team_a["current_position"];
+    $debate_level_a = $team_a["debate_level"];
+    $index_a = $team_a["index"];
+    
+    $team_a["current_position"] = $team_b["current_position"];
+    $team_a["debate_level"] = $team_b["debate_level"];
+    $team_a["index"] = $team_b["index"];
+    
+    $team_b["current_position"] = $current_position_a;
+    $team_b["debate_level"] = $debate_level_a;
+    $team_b["index"] = $index_a;
+    
+    $teams[$team_a["index"]] = $team_a;
+    $teams[$team_b["index"]] = $team_b;
 }
 
 function find_best_swap_for(&$teams, &$team_a) {
     $best_effect = 0;
     $best_team_b = false;
     foreach ($teams as $team_b) { //this loop especially can be limited
-        if (is_swappable($team_a, $team_b)) {
+        if (is_swappable(&$team_a, &$team_b)) {
             $current = team_badness($team_a) + team_badness($team_b);
             $future = team_badness($team_a, $team_b["current_position"]) + team_badness($team_b, $team_a["current_position"]);
-            if ($future == 0) {
-                swap_two_teams($team_a, $team_b);
+/*            if ($future == 0) {
+                swap_two_teams($teams, $team_a, $team_b);
                 return;
-            }
+            }*/
             $net_effect = $future - $current;
             if ($net_effect < $best_effect) {
                 $best_effect = $net_effect;
-                $best_team_b = $team_b;
+                $best_team_b = &$team_b;
             }
         }
     }
     if ($best_team_b) {
-        swap_two_teams($team_a, $team_b);
+        print "******************************\n";
+        print $team_a["team_id"]. ", " .$best_team_b["team_id"] ."\n";
+        var_dump($teams);
+        swap_two_teams(&$teams, &$team_a, &$best_team_b);
+        print "after\n";
+        var_dump($teams);
     }
 }
 
@@ -107,18 +96,18 @@ function calculate_draw($teams) {
     shuffle($teams);
     usort($teams, "cmp_teams_on_points");
     $teams = array_reverse($teams);
-    $solution = debates_from_teams($teams);
-    $teams = attributed_teams_from_debates($solution);
+    $teams = attributed_teams_from_debates(debates_from_teams($teams));
     $previous_solution = 0;
-    while (debates_badnesses($solution) > 0) {
-        if ($previous_solution == debates_badnesses($solution))
+    while (teams_badness($teams) > 0) {
+        if ($previous_solution == teams_badness($teams))
             break;
-        $previous_solution = debates_badnesses($solution);
+        $previous_solution = teams_badness($teams);
         foreach ($teams as $team)
             if (team_badness($team) > 0)
-                find_best_swap_for($teams, $team);
+                find_best_swap_for(&$teams, &$team);
     }
-    return just_ids_from_debates($solution);
+    //print "FINAL SCORE: " . teams_badness($teams);
+    return just_ids_from_debates(debates_from_teams($teams));
 }
 
 /*

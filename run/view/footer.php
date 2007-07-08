614b69076e7a6eb688b7d6187b0c4c7f6b6ed887
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-07-08 10:12:11 +0000

    Clean Step N
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/branches/clean@110 fa58ba4a-40fe-4f97-9c31-9099acd41d38

---
+++ run/view/footer.php
@@ -0,0 +1,5 @@
+</div>
+
+<?php //include('customize/footer.inc'); ?>
+</body>
+</html>
---
+++ run/view/mainmenu.php
@@ -0,0 +1,11 @@
+<div id="mainmenu">
+<h2 class="hide">Main Menu</h2>
+<ul>
+    <li><a href="input.php">Input</a></li>
+    <li><a href="draw.php">Draw</a></li>
+    <li><a href="result.php">Results</a></li>
+    <li><a href="standing.php">Standings</a></li>
+    <li><a href="print.php">Print</a></li>
+    <li><a href="setup.php">Setup</a></li>
+</ul>
+</div>

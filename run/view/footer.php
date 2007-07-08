48c2c9cefb912d5a7ce7e366953af42852286b2c
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-07-08 15:52:04 +0000

    Clean Step N - moved SourceForge logo back in
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/branches/clean@113 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- run/view/footer.php
+++ run/view/footer.php
@@ -1,5 +1,9 @@
 </div>
 
-<?php //include('customize/footer.inc'); ?>
+<div>
+<br>
+<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=199347&amp;type=3" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a>
+</div>
+
 </body>
 </html>

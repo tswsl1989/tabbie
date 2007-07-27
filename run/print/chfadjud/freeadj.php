86be1ad1fb3065192d6655bbdb2fe777ee361ec5
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-07-27 16:34:36 +0000

    BROKEN - some attempts at cleanup of the print module
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@160 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- run/print/chfadjud/freeadj.php
+++ run/print/chfadjud/freeadj.php
@@ -5,12 +5,9 @@ $query .= "LEFT JOIN temp_adjud_round_$roundno T ON A.adjud_id = T.adjud_id ";
 $query .= "WHERE T.adjud_id IS NULL AND active='Y' ORDER BY ranking DESC"; 
 
 $result = mysql_query($query);
-if ($result)
-    { 
-        $text.=" <tr><th>Adjudicator Name</th><th>Ranking</th><th>Conflicts</th>"
 
-        while ($row=mysql_fetch_assoc($result))
-        {    $adjudname=$row['adjud_name'];
-            $ranking=$row['ranking'];
-            $conflicts=$row['conflicts'];
-            $text=" <td>$adjudname</td><td>$ranking</td><td>$conflicts</td> \n";
+$header = array("Adjudicator Name", "Ranking", "Conflicts");
+$data = array();
+while ($row = mysql_fetch_assoc($result)) {
+    $data[] = array($row['adjud_name'], $row['ranking'], $row["conflicts"]);
+}
--- run/print/floor/teams.php
+++ run/print/floor/teams.php
@@ -1,17 +1,14 @@
 <?
+$header = array("Team Name", "Venue", "Venue Location")
 
-    $text.=" <tr><th>Team Name</th><th>Venue</th><th>Venue Location</th><th>&nbsp;</th> \n";
-
+$query = 
     $venuequery="SELECT v.venue_id AS venue_id, v.venue_location AS venue_location, v.venue_name AS venue_name, t.team_id AS team_id, t.team_code AS team_code, u.univ_code AS univ_code ";
     $venuequery.="FROM team AS t, university AS u, draw_round_$roundno AS d, venue AS v ";
     $venuequery.="WHERE d.venue_id=v.venue_id AND (t.team_id=d.og OR t.team_id=d.oo OR t.team_id=d.cg OR t.team_id=d.co) AND t.univ_id=u.univ_id ";
     $venuequery.="ORDER BY univ_code, team_code ";
     $venueresult=mysql_query($venuequery);
 
-    while ($venuerow=mysql_fetch_assoc($venueresult))
-    {    $venue_location = $venuerow['venue_location'];
-        $teamname = $venuerow['univ_code']." ".$venuerow['team_code'];
-        $venuename = $venuerow['venue_name']    ;
-
-        $text=" <td>$teamname</td><td>$venuename</td><td>$venue_location</td>\n";
-    }
+$data = array();
+while ($row = mysql_fetch_assoc($result)) {
+    $data[] = array($row['univ_code'] . " " . $row['team_code'], $row["venue_name"], $row["venue_location"]);
+}

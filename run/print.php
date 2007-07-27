11cf7612fe1178ebb3d8576b8862a311d4cf2b43
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-07-27 17:23:11 +0000

    BROKEN - some attempts at cleanup of the print module
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@163 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- run/print.php
+++ run/print.php
@@ -8,15 +8,23 @@ require("view/mainmenu.php");
 
 <h2>Print</h2>
 
-<h3>Runners and floormanagers</h3>
+<h3>Rooms (distribute)</h3>
 <ul>
-<li><a href="....">Personalised adjudicator sheets</a></li>
-<li><a href="input.php?moduletype=venue">Input venue (room) names and locations</a></li>
-<li><a href="input.php?moduletype=univ">Input participating universities or institutions</a></li>
-<li><a href="input.php?moduletype=adjud">Input participating adjudicators</a></li>
-<li><a href="input.php?moduletype=team">Input participating teams</a></li>
-<li><a href="input.php?moduletype=motion">Input motions for the regular rounds</a><li>
-<li><a href="backup.php">Make a backup (and save it on another computer)</a></li>
+<li><a href="adjsheet.php">Personalised adjudicator sheets</a></li>
+</ul>
+
+<h3>Runners and floormanagers (keep in hand)</h3>
+<ul>
+<li><a href="....recreate...">Printable version of the draw</a></li>
+<li><a href="floor_adj.php">List of adjudicators, and their venues</a></li>
+<li><a href="floor_teams.php">List of teams, and their venues</a>
+</ul>
+
+<h3>Tab Room (keep in the room)</h3>
+<ul>
+<li><a href="adjlist.php">Full list of adjudicators</a></li>
+<li><a href="freeadj.php">List of unassigned adjudicators</a></li>
+<li><a href="teamadjcount.php">Adjudicators per team overview</a></li>
 </ul>
 <p>
 

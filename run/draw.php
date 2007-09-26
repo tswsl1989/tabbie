bdbd313595d8da61212e0b13a9b676f8dd0dec54
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-09-26 20:19:49 +0000

    Bugfix: edit draw was not accessible for round 1
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@219 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- run/draw.php
+++ run/draw.php
@@ -28,9 +28,6 @@ $result = mysql_query($query);
 $numdraws = mysql_num_rows($result);
 $nextround = $numdraws + 1;
 
-if ($numdraws <= 0) 
-    $ntu_override_module = "currentdraw";
-
 $ntu_controller = "draw";
 $ntu_default_module = "round";
 $ntu_default_action = "";

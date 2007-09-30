0f98bfffa229a51727b1b6b074b618fdedf73de8
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-09-30 13:43:41 +0000

    Removed SF logo
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@234 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- view/footer.php
+++ view/footer.php
@@ -22,10 +22,5 @@
  * end license */ ?>
 </div>
 
-<div>
-<br>
-<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=199347&amp;type=3" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a>
-</div>
-
 </body>
 </html>

dd64a04ec9e8436b58f911b03e6923908b26c834
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-10-09 09:14:38 +0000

    Results by position / Motion Fairness added
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@243 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- results_by_position.php
+++ results_by_position.php
@@ -28,19 +28,22 @@ require("view/header.php");
 require("view/mainmenu.php");
 
 $POSITIONS = array('og' => "Opening Government", 'oo' => "Opening Opposition", 'cg' => "Closing Government", 'co' => "Closing Opposition");
-$RANKINGS = array('first', 'second', 'third', 'fourth', 'total', 'normalized');
+$RANKINGS = array('first', 'second', 'third', 'fourth', 'normalized');
 
 for ($round = 1; $round <= get_num_completed_rounds(); $round++) {
     $motion = get_motion_for_round($round);
     print "<h2>Round $round</h2>";
-    print "<h3>Motion: $motion</h3>";
+    print "<h3>Motion: $motion</h3><table><tr>";
     $results = results_by_position($round);
     foreach ($POSITIONS as $short => $long) {
+        print "<td>";
         print "<h3>$long</h3>";
         foreach ($RANKINGS as $ranking) {
             print ucwords($ranking) . ": " . $results[$short][$ranking] . "<br>";
         } 
+        print "</td>";
     }
+    print "</tr></table>";
 }
 
 require("view/footer.php");

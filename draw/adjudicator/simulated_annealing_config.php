c21697818c7c6343d5f5e8327792b06369905ced
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2007-11-20 21:20:48 +0000

    Added backwards compatability (for versions < 1.3.1) for adjudication allocation
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@274 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- draw/adjudicator/simulated_annealing_config.php
+++ draw/adjudicator/simulated_annealing_config.php
@@ -23,19 +23,38 @@
 
 //Energy calculation:
 
+function ensure_scoring_factors_in_db() {
+    //for backwards compatability with Tabbie versions < 1.3.1
+    $result = mysql_query("SHOW TABLES LIKE 'configure_adjud_draw'");
+    if (!mysql_num_rows($result))
+        mysql_query("CREATE TABLE configure_adjud_draw ( 
+  param_name varchar(100), 
+  param_value double, 
+  PRIMARY KEY (param_name) 
+)");
+
+    $values = array('university_conflict', 'team_conflict', 'chair_not_perfect', 'chair_not_ciaran_perfect', 'panel_steepness', 'panel_strength_not_perfect', 'panel_size_not_perfect', 'panel_size_out_of_bounds', 'adjudicator_met_adjudicator', 'adjudicator_met_team', 'lock');
+    foreach ($values as $value) {
+        $db_res = mysql_query("SELECT * FROM configure_adjud_draw WHERE param_name='$value'");
+        if (!(mysql_fetch_assoc($db_res)))
+            mysql_query("INSERT INTO configure_adjud_draw (param_name, param_value) VALUES ('$value', 0)");
+    }
+}
+
 function get_scoring_factors_from_db() {
-  $params = array();
-  $db_res = mysql_query("select param_name,param_value from configure_adjud_draw");
-  while ($row = mysql_fetch_assoc($db_res)) {
-    $params[$row['param_name']]=$row['param_value'];
-  }
-  return $params;
+    ensure_scoring_factors_in_db();
+    $params = array();
+    $db_res = mysql_query("SELECT param_name, param_value FROM configure_adjud_draw");
+    while ($row = mysql_fetch_assoc($db_res)) {
+        $params[$row['param_name']] = $row['param_value'];
+    }
+    return $params;
 }
 
 function store_scoring_factors_to_db($params) {
     foreach ($params as $param => $pvalue) {
-      mysql_query("update configure_adjud_draw set param_value='$pvalue' where param_name='$param'");
-  }
+        mysql_query("UPDATE configure_adjud_draw SET param_value='$pvalue' WHERE param_name='$param'");
+    }
 }
 
 $scoring_factors = get_scoring_factors_from_db();

270675076af7d30fbde5618a6584cfcb9d54d5a1
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2008-01-13 09:54:04 +0000

    First (working) sketch of new DB layout. NB CODE BROKEN FROM THIS POINT
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@327 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- install/create_db.sql
+++ install/create_db.sql
@@ -1,78 +1,198 @@
-CREATE TABLE adjudicator (
-  adjud_id mediumint(9) NOT NULL auto_increment,
-  univ_id mediumint(9) NOT NULL default '0',
-  adjud_name varchar(100) NOT NULL default '',
-  ranking mediumint(9) NOT NULL default '0',
-  active enum('Y','N') NOT NULL default 'Y',
-  conflicts varchar(100) default NULL,
-  PRIMARY KEY  (adjud_id),
-  UNIQUE KEY adjud_name (adjud_name)
-) TYPE=MyISAM COMMENT='Adjudicator Table';
+-- Tabbie, Debating Tabbing Software
+-- Copyright Contributors
+-- 
+-- This file is part of Tabbie
+-- 
+-- Tabbie is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+-- 
+-- Tabbie is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with Tabbie; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-CREATE TABLE motions (
-  round_no smallint(6) NOT NULL default '0',
-  motion text NOT NULL,
-  PRIMARY KEY  (round_no)
+CREATE TABLE tournaments (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY  (id)
+)   TYPE=MyISAM;
+
+CREATE TABLE phases (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    tournament_id MEDIUMINT UNSIGNED NOT NULL,
+    seq MEDIUMINT UNSIGNED NOT NULL,
+    name VARCHAR(100),
+    PRIMARY KEY  (id)
+)   TYPE=MyISAM;
+
+CREATE TABLE rounds (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    phase_id MEDIUMINT UNSIGNED NOT NULL,
+    seq MEDIUMINT UNSIGNED NOT NULL,
+    motion VARCHAR(2048) NOT NULL,
+    PRIMARY KEY (id)
 ) TYPE=MyISAM;
 
-CREATE TABLE speaker (
-  speaker_id mediumint(9) NOT NULL auto_increment,
-  team_id mediumint(9) NOT NULL default '0',
-  speaker_name varchar(100) NOT NULL default '',
-  PRIMARY KEY  (speaker_id),
-  UNIQUE KEY team_id (team_id,speaker_name)
+CREATE TABLE persons (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (name)
 ) TYPE=MyISAM;
 
-CREATE TABLE team (
-  team_id mediumint(9) NOT NULL auto_increment,
-  univ_id mediumint(9) NOT NULL default '0',
-  team_code varchar(20) NOT NULL default '',
-  esl enum('N','Y') NOT NULL default 'N',
-  active enum('N','Y') NOT NULL default 'N',
-  composite enum('N','Y') NOT NULL default 'Y',
-  PRIMARY KEY  (team_id),
-  UNIQUE KEY univ_id (univ_id,team_code)
-) TYPE=MyISAM COMMENT='Team Table';
-
-CREATE TABLE university (
-  univ_id mediumint(9) NOT NULL auto_increment,
-  univ_name varchar(100) NOT NULL default '',
-  univ_code varchar(20) NOT NULL default '',
-  PRIMARY KEY  (univ_id),
-  UNIQUE KEY univ_code (univ_code)
-) TYPE=MyISAM COMMENT='University Table';
-
-CREATE TABLE venue (
-  venue_id mediumint(9) NOT NULL auto_increment,
-  venue_name varchar(50) NOT NULL default '',
-  venue_location varchar(50) NOT NULL default '',
-  active enum('Y','N') NOT NULL default 'Y',
-  PRIMARY KEY  (venue_id),
-  UNIQUE KEY venue_name (venue_name)
+CREATE TABLE speakers (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    person_id MEDIUMINT UNSIGNED NOT NULL,
+    team_id MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (person_id, team_id)
 ) TYPE=MyISAM;
 
-CREATE TABLE highlight (
-  lowerlimit char(50),
-  upperlimit char(50),
-  type char(50)
+CREATE TABLE teams (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    university_id MEDIUMINT UNSIGNED NOT NULL,
+    short_name VARCHAR(20) NOT NULL,
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (university_id, short_name)
 ) TYPE=MyISAM;
 
-INSERT INTO highlight VALUES ('50', '100', 'result');
+CREATE TABLE adjudicators (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    university_id MEDIUMINT UNSIGNED NOT NULL,
+    person_id MEDIUMINT UNSIGNED NOT NULL,
+    points MEDIUMINT NOT NULL,
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (person_id)
+) TYPE=MyISAM;
 
-CREATE TABLE configure_adjud_draw ( 
-  param_name varchar(100), 
-  param_value double, 
-  PRIMARY KEY (param_name) 
-);
+CREATE TABLE team_scratches (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    adjudicator_id MEDIUMINT UNSIGNED NOT NULL,
+    team_id MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (adjudicator_id, team_id)
+) TYPE=MyISAM;
 
-INSERT INTO configure_adjud_draw values('university_conflict', 10000);
-INSERT INTO configure_adjud_draw values('team_conflict', 10000);
-INSERT INTO configure_adjud_draw values('chair_not_perfect', 0);
-INSERT INTO configure_adjud_draw values('chair_not_ciaran_perfect', 1);
-INSERT INTO configure_adjud_draw values('panel_steepness', 0.1);
-INSERT INTO configure_adjud_draw values('panel_strength_not_perfect', 1);
-INSERT INTO configure_adjud_draw values('panel_size_not_perfect', 0);
-INSERT INTO configure_adjud_draw values('panel_size_out_of_bounds', 1000);
-INSERT INTO configure_adjud_draw values('adjudicator_met_adjudicator', 0);
-INSERT INTO configure_adjud_draw values('adjudicator_met_team', 0);
-INSERT INTO configure_adjud_draw values('lock', 0);
+CREATE TABLE university_scratches (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    adjudicator_id MEDIUMINT UNSIGNED NOT NULL,
+    team_id MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (adjudicator_id, team_id)
+) TYPE=MyISAM;
+
+CREATE TABLE team_allocation (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    round_id MEDIUMINT UNSIGNED NOT NULL,
+    team_id MEDIUMINT UNSIGNED NOT NULL,
+    room_id MEDIUMINT UNSIGNED NOT NULL,
+    team_role_id MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (round_id, team_id)
+) TYPE=MyISAM;
+
+CREATE TABLE judge_allocation (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    round_id MEDIUMINT UNSIGNED NOT NULL,
+    adjudicator_id MEDIUMINT UNSIGNED NOT NULL,
+    room_id MEDIUMINT UNSIGNED NOT NULL,
+    adjudicator_role_id MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (round_id, adjudicator_id)
+) TYPE=MyISAM;
+
+CREATE TABLE team_points (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    team_id MEDIUMINT UNSIGNED NOT NULL,
+    round_id MEDIUMINT UNSIGNED NOT NULL,
+    points MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (round_id, team_id)
+) TYPE=MyISAM;
+
+CREATE TABLE speaker_points (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    speaker_id MEDIUMINT UNSIGNED NOT NULL,
+    round_id MEDIUMINT UNSIGNED NOT NULL,
+    points MEDIUMINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (round_id, speaker_id)
+) TYPE=MyISAM;
+
+CREATE TABLE team_roles (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    short_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+) TYPE=MyISAM;
+
+CREATE TABLE judge_roles (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    short_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+) TYPE=MyISAM;
+
+CREATE TABLE team_properties (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    team_id MEDIUMINT UNSIGNED NOT NULL,
+    type_id MEDIUMINT UNSIGNED NOT NULL,
+    value VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) TYPE=MyISAM;
+
+CREATE TABLE team_property_types (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    short_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+) TYPE=MyISAM;
+
+CREATE TABLE universities (
+    id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    short_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (short_name)
+) TYPE=MyISAM;
+
+CREATE TABLE rooms (
+  id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  building_id MEDIUMINT UNSIGNED NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  active BOOLEAN NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (name)
+) TYPE=MyISAM;
+
+CREATE TABLE buildings (
+  id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (name)
+) TYPE=MyISAM;
+
+CREATE TABLE configuration (
+  configuration_name VARCHAR(100),
+  configuration_value DOUBLE,
+  PRIMARY KEY (configuration_name)
+) TYPE=MyISAM;
+
+INSERT INTO configuration VALUES ('university_conflict', 10000);
+INSERT INTO configuration VALUES ('team_conflict', 10000);
+INSERT INTO configuration VALUES ('chair_not_perfect', 0);
+INSERT INTO configuration VALUES ('chair_not_ciaran_perfect', 1);
+INSERT INTO configuration VALUES ('panel_steepness', 0.1);
+INSERT INTO configuration VALUES ('panel_strength_not_perfect', 1);
+INSERT INTO configuration VALUES ('panel_size_not_perfect', 0);
+INSERT INTO configuration VALUES ('panel_size_out_of_bounds', 1000);
+INSERT INTO configuration VALUES ('adjudicator_met_adjudicator', 0);
+INSERT INTO configuration VALUES ('adjudicator_met_team', 0);
+INSERT INTO configuration VALUES ('lock', 0);

17b6e33035ef2b2339c8dffd254529f4e94b34b6
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2008-01-13 11:24:56 +0000

    Home page
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/branches/tabbie_2.0@331 fa58ba4a-40fe-4f97-9c31-9099acd41d38

---
+++ system/application/views/home/home.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Welcome to Tabbie 2.0</title>
+</head>
+<body>
+
+<h1>Welcome to Tabbie 2.0</h1>
+
+<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+</body>
+</html>

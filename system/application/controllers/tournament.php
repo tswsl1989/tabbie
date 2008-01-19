387a7aacad0f3778a7915e211566452fd61df2bf
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2008-01-19 20:30:55 +0000

    Some more steps in create tournament
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@341 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- system/application/controllers/tournament.php
+++ system/application/controllers/tournament.php
@@ -14,28 +14,39 @@ class Tournament extends Controller {
         $this->load->helper('form');
         $this->load->library('validation');
 
-        $rules['short_name'] = "required|alpha_dash|maximum_length[20]|short_name|callback_shortname_check";
+        $rules['short_name'] = "required|alpha_dash|maximum_length[20]|short_name|callback__shortname_check";
         $rules['name'] = "required|maximum_length[100]";
         $this->validation->set_rules($rules);
 
-        $fields['short_name']	= 'Identifier';
+        $fields['short_name'] = 'Identifier';
         $fields['name']	= 'Name';
         $this->validation->set_fields($fields);
 
         if ($this->validation->run() == FALSE) {
             $this->load->view('tournament/create');
         } else {
-            //create tournament
-            $this->load->view('tournament/show');
+            $this->_create();
+            redirect('tournament/show');
         }
     }
 
-    function shortname_check($username) {
-        $this->validation->set_message('shortname_check', 'This identifier is already in use.');
-        return false;
-        //return ($username == $this->session->userdata('username'));
+    function show() {
+        $this->load->view('tournament/show');
     }
 
+    function _create() {
+        $data = array(
+            'short_name' => $this->input->post('short_name'),
+            'name' => $this->input->post('name'),
+        );
+        $this->db->insert('tournaments', $data); 
+    }
+
+    function _shortname_check($username) {
+        $this->validation->set_message('shortname_check', 'This identifier is already used.');
+        $this->db->from('tournaments')->where('short_name', $this->input->post('short_name'));
+        return $this->db->get()->num_rows() == 0;
+    }
 
 }
 ?>
---
+++ system/application/views/tournament/show.php
@@ -0,0 +1,10 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<? $page_title = "..... Tournament" ?>
+<?php require APPPATH . "views/header.php"; ?>
+
+<h1>Tournament Details</h1>
+
+...
+
+<?php require APPPATH . "views/footer.php"; ?>

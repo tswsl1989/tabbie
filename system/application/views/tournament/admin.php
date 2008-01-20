24873c917fea63bbb99904e009dad9134251e396
vanschelven <vanschelven@fa58ba4a-40fe-4f97-9c31-9099acd41d38>
2008-01-20 10:00:16 +0000

    Building Add
    git-svn-id: https://tabbie.svn.sourceforge.net/svnroot/tabbie/trunk@345 fa58ba4a-40fe-4f97-9c31-9099acd41d38

--- system/application/views/tournament/admin.php
+++ system/application/views/tournament/admin.php
@@ -3,8 +3,9 @@
 <? $page_title = "{$tournament->name} - Admin" ?>
 <?php require APPPATH . "views/header.php"; ?>
 
-<h1>Tournament Admin</h1>
+<h1>Tournament Admin - <?= $tournament->name ?></h1>
 
+<a href="<?= site_url("building/overview/{$tournament->short_name}") ?>"> Building overview</a><br/>
 <a href="<?= site_url("room/overview/{$tournament->short_name}") ?>"> Room overview</a><br/>
 
 <?php require APPPATH . "views/footer.php"; ?>

f4932101d0ddf70008d23c9ee5bd5ab2bb1a5813
Thomas Lake <tswsl1989@sucs.org>
2014-02-02 18:43:26 +0000

    Substitute 'Settings' for 'adjudicator parameters'

--- view/input/menu.php
+++ view/input/menu.php
@@ -23,12 +23,12 @@
 <div id="submenu">
 <h2 class="hide">Input Submenu</h2>
     <ul>
-    <li><a href="input.php?moduletype=main" <?echo ($moduletype=="main")?"class=\"activemain\"":""?>>main</a></li>
-    <li><a href="input.php?moduletype=venue" <?echo ($moduletype=="venue")?"class=\"activemain\"":""?>>venue</a></li>
-    <li><a href="input.php?moduletype=univ" <?echo ($moduletype=="univ")?"class=\"activemain\"":""?>>university</a></li>
-    <li><a href="input.php?moduletype=adjud" <?echo ($moduletype=="adjud")?"class=\"activemain\"":""?>>adjudicator</a></li>
-    <li><a href="input.php?moduletype=team" <?echo ($moduletype=="team")?"class=\"activemain\"":""?>>team</a></li>
-    <li><a href="input.php?moduletype=motion" <?echo ($moduletype=="motion")?"class=\"activemain\"":""?>>motion</a></li>
-    <li><a href="input.php?moduletype=adjud_params" <?echo ($moduletype=="adjud_params")?"class=\"activemain\"":""?>>adjudication parameters</a></li>
+    <li><a href="input.php?moduletype=main" <?= ($moduletype=="main")?"class=\"activemain\"":""?>>main</a></li>
+    <li><a href="input.php?moduletype=venue" <?= ($moduletype=="venue")?"class=\"activemain\"":""?>>venue</a></li>
+    <li><a href="input.php?moduletype=univ" <?= ($moduletype=="univ")?"class=\"activemain\"":""?>>university</a></li>
+    <li><a href="input.php?moduletype=adjud" <?= ($moduletype=="adjud")?"class=\"activemain\"":""?>>adjudicator</a></li>
+    <li><a href="input.php?moduletype=team" <?= ($moduletype=="team")?"class=\"activemain\"":""?>>team</a></li>
+    <li><a href="input.php?moduletype=motion" <?= ($moduletype=="motion")?"class=\"activemain\"":""?>>motion</a></li>
+    <li><a href="input.php?moduletype=adjud_params" <?= ($moduletype=="adjud_params")?"class=\"activemain\"":""?>>Settings</a></li>
     </ul>
 </div>

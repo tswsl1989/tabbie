c48c817995ec2694ca08fbe5ab7ef93f76d833ac
Roberto Sarrionandia <Tito@Robertos-MacBook-Pro.local>
2015-03-08 13:29:25 +0000

    Removed short open tags from input overview page

--- input/main.php
+++ input/main.php
@@ -1,24 +1,24 @@
 <?php /* begin license *
- * 
+ *
  *     Tabbie, Debating Tabbing Software
  *     Copyright Contributors
- * 
+ *
  *     This file is part of Tabbie
- * 
+ *
  *     Tabbie is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation; either version 2 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     Tabbie is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with Tabbie; if not, write to the Free Software
  *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * end license */
 
 require_once("includes/backend.php");
@@ -65,10 +65,10 @@ $numadjudactive=$row['num'];
 
 <h2>Summary</h2>
 <ul class="summary">
-    <li><span class="flt">No. of Venues</span>: <?echo "<b>$numvenue</b> ($numvenueactive)"?></li>
-    <li><span class="flt">No. of Universities</span>: <?echo "<b>$numuniv</b>"?></li>
-    <li><span class="flt">No. of Teams</span>: <?echo "<b>$numteam</b> ($numteamactive)"?></li>
-    <li><span class="flt">No. of Adjudicators</span>: <?echo "<b>$numadjud</b> ($numadjudactive)"?></li>
+    <li><span class="flt">No. of Venues</span>: <?php echo "<b>$numvenue</b> ($numvenueactive)"?></li>
+    <li><span class="flt">No. of Universities</span>: <?php echo "<b>$numuniv</b>"?></li>
+    <li><span class="flt">No. of Teams</span>: <?php echo "<b>$numteam</b> ($numteamactive)"?></li>
+    <li><span class="flt">No. of Adjudicators</span>: <?php echo "<b>$numadjud</b> ($numadjudactive)"?></li>
 </ul>
 
 <p class="msg">* The numbers in brackets indicate how many entries are active</p>
